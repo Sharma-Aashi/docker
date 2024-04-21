bd4b43630f7b5025c8f4d6d61fa92d43e1200020
Sharma-Aashi <waytoashishsharma@gmail.com>
2024-04-21 15:11:39 +0530

    title for auto deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Angular docker version 20 EC2 Devlopment added';
+  title = 'Auto deployed using CI/CD pipeline';
 }
